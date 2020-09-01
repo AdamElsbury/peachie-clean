023f6debd2d158648b736f6008fdafef4fc9829b
adamelsbury <adam.john.elsbury@gmail.com>
2020-09-01 17:00:37 +0100

    Index added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
-function showMenu() {
-  document.getElementById("menu").style.opacity = "1";
-  document.getElementById("menu").style.zIndex = "2";
-  document.getElementById("menu").style.top = "0";
-  document.getElementById("menu").style.display = "sticky";
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+function showHideDropdown() {
+  var x = document.getElementById("navbarDropdown");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
 function closeMenu() {
